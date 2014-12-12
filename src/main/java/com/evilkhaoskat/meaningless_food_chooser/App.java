f07d63eab197ec10ce23a44b2a033f496083f6d7
Roman Garanin <git@romangaranin.net>
2014-12-12 19:47:04 +0400

    Base maven project structure

---
+++ src/main/java/com/evilkhaoskat/meaningless_food_chooser/App.java
@@ -0,0 +1,13 @@
+package com.evilkhaoskat.meaningless_food_chooser;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/evilkhaoskat/meaningless_food_chooser/AppTest.java
@@ -0,0 +1,21 @@
+package com.evilkhaoskat.meaningless_food_chooser;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    @Test
+    public void testSomething() {
+        assertEquals(true, true);
+
+    }
+}
+
+
+
