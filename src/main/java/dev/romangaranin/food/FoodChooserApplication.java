ff39ebd070ea07a01b6000801223306be51fa2bd
Roman Garanin <git@romangaranin.net>
2020-06-27 18:36:26 +0300

    feat: add CheckStyle, SpotBugs, MapStruct

---
+++ src/main/java/dev/romangaranin/food/FoodChooserApplication.java
@@ -0,0 +1,16 @@
+package dev.romangaranin.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Food Chooser main class.
+ */
+@SpringBootApplication
+public class FoodChooserApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(FoodChooserApplication.class, args);
+    }
+
+}
--- src/main/java/dev/romangaranin/food/controller/v1/CustomController.java
+++ src/main/java/dev/romangaranin/food/controller/v1/CustomController.java
@@ -1,9 +1,14 @@
-package dev.romangaranin.food.controllers;
+package dev.romangaranin.food.controller.v1;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Test controller to trigger OpenAPI integration.
+ */
 @RestController
+@RequestMapping(path = "/api/v1")
 public class CustomController {
 
     @GetMapping("/custom")
